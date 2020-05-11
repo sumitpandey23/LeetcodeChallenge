8144f68eff0a2a4677103b05d1152c5fcc5e1644
SUMIT PANDEY <sumitp2398@gmail.com>
2020-05-11 23:16:05 +0530

    Update Day7.py

--- MayChallenge/Day7.py
+++ MayChallenge/Day7.py
@@ -33,3 +33,22 @@ The number of nodes in the tree will be between 2 and 100.
 Each node has a unique integer value from 1 to 100.
 
 """
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        arr = [[0, 0] for _ in range(101)]        
+        def traverse(r, depth, pv):
+            if not r: return 
+            arr[r.val] = [depth, pv]
+            traverse(r.left, depth + 1, r.val)
+            traverse(r.right, depth + 1, r.val)            
+        
+        traverse(root, 0, -1)
+        return arr[x][0] == arr[y][0] and arr[x][1] != arr[y][1]
+        
