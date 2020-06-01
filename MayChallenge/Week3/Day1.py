76d027122bc720eb413ebc9590d5c6df5de09080
SUMIT PANDEY <sumitp2398@gmail.com>
2020-06-01 23:24:09 +0530

    Update Day1.py

--- MayChallenge/Week3/Day1.py
+++ MayChallenge/Week3/Day1.py
@@ -1,4 +1,70 @@
 """
+Maximum Sum Circular Subarray
 
+Given a circular array C of integers represented by A, find the maximum possible sum of a non-empty subarray of C.
+
+Here, a circular array means the end of the array connects to the beginning of the array.  (Formally, C[i] = A[i] when 0 <= i < A.length, and C[i+A.length] = C[i] when i >= 0.)
+
+Also, a subarray may only include each element of the fixed buffer A at most once.  (Formally, for a subarray C[i], C[i+1], ..., C[j], there does not exist i <= k1, k2 <= j with k1 % A.length = k2 % A.length.)
+
+ 
+
+Example 1:
+
+Input: [1,-2,3,-2]
+Output: 3
+Explanation: Subarray [3] has maximum sum 3
+Example 2:
+
+Input: [5,-3,5]
+Output: 10
+Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10
+Example 3:
+
+Input: [3,-1,2,-1]
+Output: 4
+Explanation: Subarray [2,-1,3] has maximum sum 2 + (-1) + 3 = 4
+Example 4:
+
+Input: [3,-2,2,-3]
+Output: 3
+Explanation: Subarray [3] and [3,-2,2] both have maximum sum 3
+Example 5:
+
+Input: [-2,-3,-1]
+Output: -1
+Explanation: Subarray [-1] has maximum sum -1
+ 
+
+Note:
+
+-30000 <= A[i] <= 30000
+1 <= A.length <= 30000
 
 """
+
+
+class Solution:
+    def maxSubarraySumCircular(self, A: List[int]) -> int:
+        k=self.kadane(A)
+        
+        
+        c=0
+        for i in range(len(A)):
+            c+=A[i]
+            A[i]=-A[i]
+        c=c+self.kadane(A)
+        
+            
+        if c>k and c!=0:
+            return c
+        else:
+            return k
+        
+    def kadane(self, num):
+        cs,ms=num[0], num[0]
+        for i in num[1:]:
+            cs=max(i,cs+i)
+            ms=max(cs, ms) 
+        return ms
+            
